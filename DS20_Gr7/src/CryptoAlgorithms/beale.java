d99be0d20a254e0ab7a150828289b158a8c6e7f1
Albiinn <56487491+Albiinn@users.noreply.github.com>
2020-03-16 17:15:52 +0100

    commit

---
+++ DS20_Gr7/src/CryptoAlgorithms/beale.java
@@ -0,0 +1,102 @@
+package CryptoAlgorithms;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class beale {
+	public static void encrypt(String path, String plaintext) throws Exception {
+		Scanner input=null;
+		String emri=null;
+		int rez=1;
+		char[] letter=plaintext.toCharArray();
+		
+		File file = new File(path);
+			
+		if(file.exists()) {
+				System.out.println("Fajlli ekzsiton (provo emer tjeter)");
+				System.exit(2);
+			}
+		
+			PrintWriter output = new PrintWriter(file);	
+			output.println("Korona arriti pothuajse ne cdo vend. Bartes i virusit eshte lakuriqi, maceja etj.");
+			output.println("Heshtja dhe qetesia kane kapluar te gjitha vendet");
+			output.println("Virusi sulmon organet e frymemarrjes");
+			output.println("Nese hundet ju rrjedhin, atehere jeni infektuar");
+			output.println("Jane shfaqur kater personat e pare te infektuar, me inicialet ZY, YZ, XU, UX");
+			output.close();
+			
+		
+			int[] cipher = new int[letter.length];
+			
+			for(int i=0; i<letter.length; i++) {
+				input=new Scanner(file);
+				
+				while(input.hasNext()) {
+				emri=input.next().toLowerCase();
+				
+				if(letter[i]==emri.charAt(0)) {
+					cipher[i]=rez;
+					input.nextLine();
+					rez=1;
+					System.out.print(cipher[i]+" ");
+					break;
+					
+				}
+				else {
+					rez=rez+1;
+				}
+			}	
+		}
+			input.close();
+	};
+	
+	
+	public static void decrypt(String path, String ciphertext) throws Exception {
+		String[] a = ciphertext.split(" ");
+		int[] numbers=new int[a.length];
+		
+		for(int i=0; i<a.length; i++) {
+			numbers[i]=Integer.parseInt(a[i]);
+		}
+		
+		File file = new File(path);
+		
+		if(file.exists()) {
+			System.out.println("Fajlli ekziston (provo emer tjeter)");
+			System.exit(2);
+		}
+		
+		PrintWriter output = new PrintWriter(file);	
+		output.println("Korona arriti pothuajse ne cdo vend. Bartes i virusit eshte lakuriqi, maceja etj.");
+		output.println("Heshtja dhe qetesia kane kapluar te gjitha vendet");
+		output.println("Virusi sulmon organet e frymemarrjes");
+		output.println("Nese hundet ju rrjedhin, atehere jeni infektuar");
+		output.println("Jane shfaqur kater personat e pare te infektuar, me inicialet ZY, YZ, XU, UX");
+		output.close();
+		
+		char[] decipher = new char[numbers.length];
+		int rez=1;
+		Scanner input=null;
+		
+		for(int i=0; i<numbers.length; i++) {
+			input = new Scanner(file);
+			
+			while(input.hasNext()) {
+				String emri = input.next().toLowerCase();
+				
+				if(numbers[i]==rez) {
+					decipher[i]=emri.charAt(0);
+					System.out.print(decipher[i]);
+					rez=1;
+					input.nextLine();
+					break;
+				}
+				else {
+					rez=rez+1;
+				}
+			}
+		}
+		input.close();
+	};
+}
