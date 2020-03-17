86d409c5e6172ca24843951feb5ad31766a37dbd
AndiHyseni <hyseniandi6@gmail.com>
2020-03-16 19:17:48 -0700

    komiti1

---
+++ DS20_Gr7/src/CryptoAlgorithms/tap_code.java
@@ -0,0 +1,73 @@
+package CryptoAlgorithms;
+
+class tap_code {
+	
+	public static void encode(String plaintext) {
+		char[][] tabela=matrica();
+		String s=plaintext.toLowerCase(); 
+		char[] letter=s.toCharArray();
+		
+		for(int k=0; k<letter.length; k++) {
+			
+			for(int i=0; i<5; i++) {
+				
+				for(int j=0; j<5; j++) {
+					
+					if(letter[k]==tabela[i][j]) {
+						
+						for(int a=0; a<i+1; a++) {
+							System.out.print(".");
+						}
+						
+						System.out.print(" ");
+						
+						for(int b=0; b<j+1; b++) {
+							System.out.print(".");
+						}
+						
+						System.out.print("   ");
+						break;
+					}
+				}
+			}
+			
+		}
+	};
+	
+	
+	public static void decode(String ciphertext) {
+		char[][] tabela=matrica();
+		String[] rreshti=ciphertext.split("  ");
+		for(int i=0; i<rreshti.length; i++) {
+			String[] kolona=rreshti[i].split(" ");		
+			System.out.print(tabela[kolona[0].length()-1][kolona[1].length()-1]);
+		}
+		
+	};
+	
+	
+	private static char[][] matrica() {
+		char[][] tabela = new char[5][5];
+		int ascci=97;
+		
+		for(int i=0; i<5; i++) {
+			
+			for(int j=0; j<5; j++) {
+				
+				if(ascci==99) {
+					tabela[i][j]=(char)107;
+				}
+				
+				else if(ascci==107) {
+					j=j-1;
+				}
+				
+				else {
+				tabela[i][j]=(char)ascci;
+				}
+				ascci=ascci+1;
+		 }
+	  }
+		return tabela;
+	}
+}
